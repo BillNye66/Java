a04d6b73d7a9cd67e214a988ae5bf863b999c67f
404kevnotfound <student@HSiMac4-124.local>
2020-02-24 16:15:16 -0700

    message

--- Ch2/E2_1.java
+++ Ch2/E2_1.java
@@ -1,11 +1,15 @@
+// Raymond Fisher
+// 02/24/2020
+// This program asks the user for an input of a temperature in celsius and gives the temperature in fahrenheit back.
+
 import java.util.Scanner;
 
 class E2_1 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a degree in Celsius: ");
+		System.out.print("Enter a degree in Celsius: ");
 		double celsius = input.nextDouble();
-		fahrenheit = (9/5) * celsius + 32;
-		System.out.print(fahrenheit);
+		double fahrenheit = (9.0/5) * celsius + 32;
+		System.out.print(celsius + " Celsius is " + fahrenheit + " Fahrenheit");
 	}
 }
---
+++ Ch2/E2_13.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class E2_13 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter monthly saving amount: ");
+		double input = input.nextDouble();
+		
+	}
+}
